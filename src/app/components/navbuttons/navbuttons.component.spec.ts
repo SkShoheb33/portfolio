7e4abe9c393be6017828a3ea01f5ef0cdbead50e
SkShoheb33 <shaikshoheb9k@gmail.com>
2022-11-05 22:58:11 +0530

    This is portfolio version 1

---
+++ src/app/components/navbuttons/navbuttons.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavbuttonsComponent } from './navbuttons.component';
+
+describe('NavbuttonsComponent', () => {
+  let component: NavbuttonsComponent;
+  let fixture: ComponentFixture<NavbuttonsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavbuttonsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NavbuttonsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/navbuttons/navbuttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbuttons',
+  templateUrl: './navbuttons.component.html',
+  styleUrls: ['./navbuttons.component.css']
+})
+export class NavbuttonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
